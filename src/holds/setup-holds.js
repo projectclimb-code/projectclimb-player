4e842bc3c431f3e42970f760bb7e410fda29ad61
Jernej Lunder <lundeee@gmail.com>
2025-11-21 08:27:17 +0100

    turn off inactiva

--- src/holds/setup-holds.js
+++ src/holds/setup-holds.js
@@ -65,7 +65,7 @@ const styles = {
     fill: 'rgba(255, 255, 255, 0)',
     stroke: 'rgba(255, 255, 255, 1)',
     strokeWidth: 12,
-    opacity: 0.1,
+    opacity: 0,
   },
   normal: {
     fill: '#ffffff77',
