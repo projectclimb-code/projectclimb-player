a0fac9978bbe44e53e46da206628cb783e91c367
Jernej Lunder <lundeee@gmail.com>
2026-03-27 17:54:25 +0100

    *

--- src/utils/scale-to-screen.js
+++ src/utils/scale-to-screen.js
@@ -3,7 +3,7 @@ export function scaleToScreen(node, settings, zoom = 0.74) {
   node.offsetY(node.height() / 2)
   node.position({
     x: settings.screenWidth / 2 - 160,
-    y: settings.screenHeight / 2 - 70,
+    y: settings.screenHeight / 2 - 60,
   })
   node.scale({
     x: (settings.screenHeight / settings.wallHeight) * zoom,
